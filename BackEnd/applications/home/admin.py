b482d1cfd8b485c5b39701f00f92cfb289b0739a
mixael442 <misaelcapo720@gmail.com>
2022-12-07 14:23:27 -0300

    rearmar backend

--- BackEnd/applications/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from .models import Proyecto, ProyectoUsuario
-admin.site.register(Proyecto)
-admin.site.register(ProyectoUsuario)
-
-# Register your models here.
